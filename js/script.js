1fabaa59288b4a3c0e21d066a2342da335bced04
AA <alexdev73@gmail.com>
2021-11-15 19:08:19 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
-
-// Or with jQuery
-
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
-      
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
